1064713e1bbc6c20c74716e3d5761bfd2768e760
Zac Flamig <zac@weatherwary.com>
2019-02-03 13:15:03 -0600

    Update graph_validators.py

--- gdcdatamodel/validators/graph_validators.py
+++ gdcdatamodel/validators/graph_validators.py
@@ -10,7 +10,7 @@ def clean_count(q):
     Args:
         q (psqlgraph.query.GraphQuery): The current query object.
     """
-    query_count = q.options(sa.orm.lazyload('*')).statement.with_only_columns([sa.func.count()]).order_by(None)
+    query_count = q.options(sqlalchemy.orm.lazyload('*')).statement.with_only_columns([sqlalchemy.func.count()]).order_by(None)
     return q.session.execute(query_count).scalar()
 
 class GDCGraphValidator(object):
