fed6ee0c394428dca8ad15954f6be32ef72b57fd
BinamB <numb.baj@gmail.com>
2024-04-15 15:58:29 -0500

    port

--- test/ci_setup.sh
+++ test/ci_setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-sudo mkdir /var/pgsql_socket/
-sudo ln -s /private/tmp/.s.PGSQL.5432 /var/pgsql_socket/
+rm /usr/local/var/postgres/postmaster.pid
 poetry run python bin/destroy_and_setup_psqlgraph.py
 # sudo service postgresql restart 13
 # openssl genrsa -out test_private_key.pem 2048
