1eb46d889ff048719d9f61d785ee04f641a3acb0
BinamB <numb.baj@gmail.com>
2024-04-16 11:11:18 -0500

    port

--- test/ci_commands_script.sh
+++ test/ci_commands_script.sh
@@ -6,5 +6,6 @@ set -e
 
 # datadict and datadictwithobjid tests must run separately to allow
 # loading different datamodels
+export POSTGRES_PORT=5432
 poetry run python bin/destroy_and_setup_psqlgraph.py
 poetry run pytest -vv --cov=gdcdatamodel --cov-report xml tests
