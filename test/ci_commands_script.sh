5724151502f5b713a17f20bb071f98f9b9a5f98e
BinamB <numb.baj@gmail.com>
2024-04-16 11:51:23 -0500

    setup

--- test/ci_commands_script.sh
+++ test/ci_commands_script.sh
@@ -6,9 +6,5 @@ set -e
 
 # datadict and datadictwithobjid tests must run separately to allow
 # loading different datamodels
-ls -al /etc/postgresql/13/main/
-# travis with postgres 13 defaults port to 5433 and test looks for port 5432
-sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/13/main/postgresql.conf
-sudo service postgresql restart 13
-poetry run python bin/destroy_and_setup_psqlgraph.py
+
 poetry run pytest -vv --cov=gdcdatamodel --cov-report xml tests
--- test/ci_setup.sh
+++ test/ci_setup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+ls -al /etc/postgresql/13/main/
+# travis with postgres 13 defaults port to 5433 and test looks for port 5432
+sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/13/main/postgresql.conf
+sudo service postgresql restart 13
 poetry run python bin/destroy_and_setup_psqlgraph.py
 # sudo service postgresql restart 13
 # openssl genrsa -out test_private_key.pem 2048
