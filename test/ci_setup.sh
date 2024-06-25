29511b4d7c96d55ea68eac2219ab74007433073e
George Thomas <george42@uchicago.edu>
2024-06-25 13:36:06 -0700

    (PPS-710): add diagnostic to ci setup script

--- test/ci_setup.sh
+++ test/ci_setup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 set -e
+
+echo "Checking version of database"
+psql -U postgres -c 'SELECT version()'
+
 poetry run python bin/destroy_and_setup_psqlgraph.py --user postgres --password postgres
